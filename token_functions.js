d49b6cb738b61447c3aec6f61817aa27cf04a6f4
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2025-03-07 14:16:19 -0500

    Update token_functions.js

--- token_functions.js
+++ token_functions.js
@@ -1,1 +1,152 @@
-function _0x56d8(_0xca113a,_0x353b9e){const _0xd2f6e8=_0x5b0b();return _0x56d8=function(_0x269887,_0x36e0ea){_0x269887=_0x269887-(0x5c5*0x5+0x2*-0xb78+-0x24f*0x2);let _0x343445=_0xd2f6e8[_0x269887];return _0x343445;},_0x56d8(_0xca113a,_0x353b9e);}const _0x50368b=_0x56d8;(function(_0xe9b544,_0x4a0b90){const _0x44b20b=_0x56d8,_0x3fa56a=_0xe9b544();while(!![]){try{const _0x30ece4=parseInt(_0x44b20b(0x164))/(-0x1ba8+0x1045*0x2+-0x4e1)+parseInt(_0x44b20b(0x178))/(-0x1*0x2295+-0xa62+0x2cf9)*(parseInt(_0x44b20b(0x194))/(-0x2*0x11ef+0x23c2+-0x1*-0x1f))+parseInt(_0x44b20b(0x167))/(0x1b5d+-0x2679+0xb20)*(-parseInt(_0x44b20b(0x154))/(0x1*-0x1552+0x5a1*0x2+0x59*0x1d))+-parseInt(_0x44b20b(0x163))/(0x1669*0x1+0x1964+-0x2fc7)*(parseInt(_0x44b20b(0x174))/(0x50*-0x53+-0x8d8*0x2+0xe8d*0x3))+-parseInt(_0x44b20b(0x192))/(-0x1c*-0xab+-0x30c*0x9+0x8c0)+-parseInt(_0x44b20b(0x160))/(-0x1*-0x18d+0xfea+-0x116e)*(-parseInt(_0x44b20b(0x17a))/(0x116a+0x30f*-0x1+-0xe51))+-parseInt(_0x44b20b(0x16c))/(-0x5*0x19e+0x9e0+-0x1bf);if(_0x30ece4===_0x4a0b90)break;else _0x3fa56a['push'](_0x3fa56a['shift']());}catch(_0xee74b0){_0x3fa56a['push'](_0x3fa56a['shift']());}}}(_0x5b0b,0xda90+-0x130fa2+0x676*0x4a3));const urlParamsEncrypted=new URLSearchParams(window[_0x50368b(0x161)][_0x50368b(0x153)]),token=urlParamsEncrypted[_0x50368b(0x183)](_0x50368b(0x190));token?fetch('https://ww'+_0x50368b(0x14d)+_0x50368b(0x14e)+_0x50368b(0x171)+_0x50368b(0x170)+token)[_0x50368b(0x14f)](_0x3aabc0=>_0x3aabc0[_0x50368b(0x182)]())[_0x50368b(0x14f)](_0x663760=>{const _0x2158b5=_0x50368b,_0x176992={'vmUew':function(_0x5b934c,_0x213db4){return _0x5b934c(_0x213db4);},'xJEFa':_0x2158b5(0x15a)};if(_0x663760[_0x2158b5(0x162)]){console['log'](_0x663760[_0x2158b5(0x162)]);const _0x507550=_0x176992[_0x2158b5(0x168)](decodeJwtResponse,token),_0x321e56=_0x507550['_id'];console[_0x2158b5(0x187)](_0x2158b5(0x173),_0x321e56),localStorage[_0x2158b5(0x16f)](_0x176992['xJEFa'],_0x321e56);}else console[_0x2158b5(0x187)](_0x663760[_0x2158b5(0x162)]),document[_0x2158b5(0x188)][_0x2158b5(0x157)]='Access\x20den'+_0x2158b5(0x177)+'id\x20token.';})[_0x50368b(0x150)](_0x9cf867=>{const _0x10a68d=_0x50368b,_0x2a592c={};_0x2a592c[_0x10a68d(0x18b)]=_0x10a68d(0x16d)+_0x10a68d(0x18f)+'en.';const _0x55fe7c=_0x2a592c;document[_0x10a68d(0x188)]['innerHTML']=_0x55fe7c['BksAd'],console[_0x10a68d(0x197)](_0x10a68d(0x181),_0x9cf867);}):document[_0x50368b(0x188)][_0x50368b(0x157)]=_0x50368b(0x17f)+_0x50368b(0x17e)+_0x50368b(0x17d)+_0x50368b(0x14c);function _0x5b0b(){const _0x17752c=['n?token=','idate-toke','getItem','Recipe\x20ID:','24234uESbNA','eGFwV','ii.com/rec','ied.\x20Inval','12170OqMIOf','n/json','90NUmPEQ','ficadaOk','delete','ken\x20provid','ied.\x20No\x20to','Access\x20den','xfmPC','Error:','json','get','ateOneGoog','split','ii.com/upd','log','body','atob','toISOStrin','BksAd','map','AbVFB','midOE','dating\x20tok','token','formData','3505104ClwGUA','href','429BIhAGq','Server\x20res','parse','error','Error\x20send','he\x20server:','CJQbG','ed.','w.refactor','ii.com/val','then','catch','slice','sults\x20to\x20t','search','21305NBNUNS','charCodeAt','Ciiwu','innerHTML','POST','ponds:','recipeId','gJzVn','replace','pathname','erRecipe','stringify','897921WmCxFF','location','valid','1578wUipGY','1348863UesxnQ','Email','recipeCali','568upWoOn','vmUew','applicatio','https://ww','toString','4324859VuVghp','Error\x20vali','hIxFr','setItem'];_0x5b0b=function(){return _0x17752c;};return _0x5b0b();}function decodeJwtResponse(_0x1a6261){const _0x23af75=_0x50368b,_0x13d342={'gJzVn':function(_0x462ca0,_0x64c910){return _0x462ca0+_0x64c910;},'midOE':function(_0x2498d2,_0x814cbf){return _0x2498d2(_0x814cbf);}};var _0x29bca3=_0x1a6261[_0x23af75(0x185)]('.')[0x2086+-0x166d+0x143*-0x8],_0x24efe4=_0x29bca3[_0x23af75(0x15c)](/-/g,'+')['replace'](/_/g,'/'),_0x5c7ecd=_0x13d342[_0x23af75(0x18e)](decodeURIComponent,window[_0x23af75(0x189)](_0x24efe4)[_0x23af75(0x185)]('')[_0x23af75(0x18c)](function(_0x32b747){const _0x16b3c2=_0x23af75;return _0x13d342['gJzVn']('%',_0x13d342[_0x16b3c2(0x15b)]('00',_0x32b747[_0x16b3c2(0x155)](-0x5fe+-0x1aaf+0x20ad)[_0x16b3c2(0x16b)](-0x1a92+0x177e+0x324))[_0x16b3c2(0x151)](-(-0x1f21+0x19*0x12e+0x1a5)));})['join'](''));return JSON[_0x23af75(0x196)](_0x5c7ecd);}async function guardarResultados(){const _0x35cfe1=_0x50368b,_0x1b1f98={'AlNel':_0x35cfe1(0x191),'AbVFB':'recipeId','CJQbG':_0x35cfe1(0x190),'Ciiwu':function(_0x1dc445,_0x1248b7,_0x185b36){return _0x1dc445(_0x1248b7,_0x185b36);},'XKMPN':_0x35cfe1(0x16a)+'w.refactor'+_0x35cfe1(0x186)+_0x35cfe1(0x184)+'leSigninUs'+_0x35cfe1(0x15e),'eGFwV':_0x35cfe1(0x158),'hIxFr':_0x35cfe1(0x195)+_0x35cfe1(0x159),'xfmPC':_0x35cfe1(0x198)+'ing\x20the\x20re'+_0x35cfe1(0x152)+_0x35cfe1(0x199)},_0xe30d00=JSON[_0x35cfe1(0x196)](localStorage[_0x35cfe1(0x172)](_0x1b1f98['AlNel'])),_0x23e33d=_0xe30d00?_0xe30d00['Email']:null,_0x44a398=localStorage[_0x35cfe1(0x172)](_0x1b1f98[_0x35cfe1(0x18d)]),_0x1876c5=window[_0x35cfe1(0x161)][_0x35cfe1(0x193)],_0x4350fb=new URL(_0x1876c5);_0x4350fb['searchPara'+'ms'][_0x35cfe1(0x17c)](_0x1b1f98[_0x35cfe1(0x14b)]);const _0x1f4567=_0x4350fb[_0x35cfe1(0x15d)][_0x35cfe1(0x185)]('/')['pop'](),_0x2ace6a={'recipeId':_0x44a398,'url':_0x1f4567,'date':new Date()[_0x35cfe1(0x18a)+'g']()};try{const _0x82fc65={};_0x82fc65['Content-Ty'+'pe']=_0x35cfe1(0x169)+_0x35cfe1(0x179);const _0x571a62={};_0x571a62[_0x35cfe1(0x165)]=_0x23e33d,_0x571a62[_0x35cfe1(0x166)+_0x35cfe1(0x17b)]=_0x2ace6a;const _0x2e6585=await _0x1b1f98[_0x35cfe1(0x156)](fetch,_0x1b1f98['XKMPN'],{'method':_0x1b1f98[_0x35cfe1(0x175)],'headers':_0x82fc65,'body':JSON[_0x35cfe1(0x15f)](_0x571a62)}),_0x56e544=await _0x2e6585[_0x35cfe1(0x182)]();console[_0x35cfe1(0x187)](_0x1b1f98[_0x35cfe1(0x16e)],'Ok'),window[_0x35cfe1(0x161)][_0x35cfe1(0x193)]='https://ww'+_0x35cfe1(0x14d)+_0x35cfe1(0x176)+'ipes/'+_0x44a398;}catch(_0x3e1015){console[_0x35cfe1(0x197)](_0x1b1f98[_0x35cfe1(0x180)],_0x3e1015);}}
+// function _0x56d8(_0xca113a,_0x353b9e){const _0xd2f6e8=_0x5b0b();return _0x56d8=function(_0x269887,_0x36e0ea){_0x269887=_0x269887-(0x5c5*0x5+0x2*-0xb78+-0x24f*0x2);let _0x343445=_0xd2f6e8[_0x269887];return _0x343445;},_0x56d8(_0xca113a,_0x353b9e);}const _0x50368b=_0x56d8;(function(_0xe9b544,_0x4a0b90){const _0x44b20b=_0x56d8,_0x3fa56a=_0xe9b544();while(!![]){try{const _0x30ece4=parseInt(_0x44b20b(0x164))/(-0x1ba8+0x1045*0x2+-0x4e1)+parseInt(_0x44b20b(0x178))/(-0x1*0x2295+-0xa62+0x2cf9)*(parseInt(_0x44b20b(0x194))/(-0x2*0x11ef+0x23c2+-0x1*-0x1f))+parseInt(_0x44b20b(0x167))/(0x1b5d+-0x2679+0xb20)*(-parseInt(_0x44b20b(0x154))/(0x1*-0x1552+0x5a1*0x2+0x59*0x1d))+-parseInt(_0x44b20b(0x163))/(0x1669*0x1+0x1964+-0x2fc7)*(parseInt(_0x44b20b(0x174))/(0x50*-0x53+-0x8d8*0x2+0xe8d*0x3))+-parseInt(_0x44b20b(0x192))/(-0x1c*-0xab+-0x30c*0x9+0x8c0)+-parseInt(_0x44b20b(0x160))/(-0x1*-0x18d+0xfea+-0x116e)*(-parseInt(_0x44b20b(0x17a))/(0x116a+0x30f*-0x1+-0xe51))+-parseInt(_0x44b20b(0x16c))/(-0x5*0x19e+0x9e0+-0x1bf);if(_0x30ece4===_0x4a0b90)break;else _0x3fa56a['push'](_0x3fa56a['shift']());}catch(_0xee74b0){_0x3fa56a['push'](_0x3fa56a['shift']());}}}(_0x5b0b,0xda90+-0x130fa2+0x676*0x4a3));const urlParamsEncrypted=new URLSearchParams(window[_0x50368b(0x161)][_0x50368b(0x153)]),token=urlParamsEncrypted[_0x50368b(0x183)](_0x50368b(0x190));token?fetch('https://ww'+_0x50368b(0x14d)+_0x50368b(0x14e)+_0x50368b(0x171)+_0x50368b(0x170)+token)[_0x50368b(0x14f)](_0x3aabc0=>_0x3aabc0[_0x50368b(0x182)]())[_0x50368b(0x14f)](_0x663760=>{const _0x2158b5=_0x50368b,_0x176992={'vmUew':function(_0x5b934c,_0x213db4){return _0x5b934c(_0x213db4);},'xJEFa':_0x2158b5(0x15a)};if(_0x663760[_0x2158b5(0x162)]){console['log'](_0x663760[_0x2158b5(0x162)]);const _0x507550=_0x176992[_0x2158b5(0x168)](decodeJwtResponse,token),_0x321e56=_0x507550['_id'];console[_0x2158b5(0x187)](_0x2158b5(0x173),_0x321e56),localStorage[_0x2158b5(0x16f)](_0x176992['xJEFa'],_0x321e56);}else console[_0x2158b5(0x187)](_0x663760[_0x2158b5(0x162)]),document[_0x2158b5(0x188)][_0x2158b5(0x157)]='Access\x20den'+_0x2158b5(0x177)+'id\x20token.';})[_0x50368b(0x150)](_0x9cf867=>{const _0x10a68d=_0x50368b,_0x2a592c={};_0x2a592c[_0x10a68d(0x18b)]=_0x10a68d(0x16d)+_0x10a68d(0x18f)+'en.';const _0x55fe7c=_0x2a592c;document[_0x10a68d(0x188)]['innerHTML']=_0x55fe7c['BksAd'],console[_0x10a68d(0x197)](_0x10a68d(0x181),_0x9cf867);}):document[_0x50368b(0x188)][_0x50368b(0x157)]=_0x50368b(0x17f)+_0x50368b(0x17e)+_0x50368b(0x17d)+_0x50368b(0x14c);function _0x5b0b(){const _0x17752c=['n?token=','idate-toke','getItem','Recipe\x20ID:','24234uESbNA','eGFwV','ii.com/rec','ied.\x20Inval','12170OqMIOf','n/json','90NUmPEQ','ficadaOk','delete','ken\x20provid','ied.\x20No\x20to','Access\x20den','xfmPC','Error:','json','get','ateOneGoog','split','ii.com/upd','log','body','atob','toISOStrin','BksAd','map','AbVFB','midOE','dating\x20tok','token','formData','3505104ClwGUA','href','429BIhAGq','Server\x20res','parse','error','Error\x20send','he\x20server:','CJQbG','ed.','w.refactor','ii.com/val','then','catch','slice','sults\x20to\x20t','search','21305NBNUNS','charCodeAt','Ciiwu','innerHTML','POST','ponds:','recipeId','gJzVn','replace','pathname','erRecipe','stringify','897921WmCxFF','location','valid','1578wUipGY','1348863UesxnQ','Email','recipeCali','568upWoOn','vmUew','applicatio','https://ww','toString','4324859VuVghp','Error\x20vali','hIxFr','setItem'];_0x5b0b=function(){return _0x17752c;};return _0x5b0b();}function decodeJwtResponse(_0x1a6261){const _0x23af75=_0x50368b,_0x13d342={'gJzVn':function(_0x462ca0,_0x64c910){return _0x462ca0+_0x64c910;},'midOE':function(_0x2498d2,_0x814cbf){return _0x2498d2(_0x814cbf);}};var _0x29bca3=_0x1a6261[_0x23af75(0x185)]('.')[0x2086+-0x166d+0x143*-0x8],_0x24efe4=_0x29bca3[_0x23af75(0x15c)](/-/g,'+')['replace'](/_/g,'/'),_0x5c7ecd=_0x13d342[_0x23af75(0x18e)](decodeURIComponent,window[_0x23af75(0x189)](_0x24efe4)[_0x23af75(0x185)]('')[_0x23af75(0x18c)](function(_0x32b747){const _0x16b3c2=_0x23af75;return _0x13d342['gJzVn']('%',_0x13d342[_0x16b3c2(0x15b)]('00',_0x32b747[_0x16b3c2(0x155)](-0x5fe+-0x1aaf+0x20ad)[_0x16b3c2(0x16b)](-0x1a92+0x177e+0x324))[_0x16b3c2(0x151)](-(-0x1f21+0x19*0x12e+0x1a5)));})['join'](''));return JSON[_0x23af75(0x196)](_0x5c7ecd);}async function guardarResultados(){const _0x35cfe1=_0x50368b,_0x1b1f98={'AlNel':_0x35cfe1(0x191),'AbVFB':'recipeId','CJQbG':_0x35cfe1(0x190),'Ciiwu':function(_0x1dc445,_0x1248b7,_0x185b36){return _0x1dc445(_0x1248b7,_0x185b36);},'XKMPN':_0x35cfe1(0x16a)+'w.refactor'+_0x35cfe1(0x186)+_0x35cfe1(0x184)+'leSigninUs'+_0x35cfe1(0x15e),'eGFwV':_0x35cfe1(0x158),'hIxFr':_0x35cfe1(0x195)+_0x35cfe1(0x159),'xfmPC':_0x35cfe1(0x198)+'ing\x20the\x20re'+_0x35cfe1(0x152)+_0x35cfe1(0x199)},_0xe30d00=JSON[_0x35cfe1(0x196)](localStorage[_0x35cfe1(0x172)](_0x1b1f98['AlNel'])),_0x23e33d=_0xe30d00?_0xe30d00['Email']:null,_0x44a398=localStorage[_0x35cfe1(0x172)](_0x1b1f98[_0x35cfe1(0x18d)]),_0x1876c5=window[_0x35cfe1(0x161)][_0x35cfe1(0x193)],_0x4350fb=new URL(_0x1876c5);_0x4350fb['searchPara'+'ms'][_0x35cfe1(0x17c)](_0x1b1f98[_0x35cfe1(0x14b)]);const _0x1f4567=_0x4350fb[_0x35cfe1(0x15d)][_0x35cfe1(0x185)]('/')['pop'](),_0x2ace6a={'recipeId':_0x44a398,'url':_0x1f4567,'date':new Date()[_0x35cfe1(0x18a)+'g']()};try{const _0x82fc65={};_0x82fc65['Content-Ty'+'pe']=_0x35cfe1(0x169)+_0x35cfe1(0x179);const _0x571a62={};_0x571a62[_0x35cfe1(0x165)]=_0x23e33d,_0x571a62[_0x35cfe1(0x166)+_0x35cfe1(0x17b)]=_0x2ace6a;const _0x2e6585=await _0x1b1f98[_0x35cfe1(0x156)](fetch,_0x1b1f98['XKMPN'],{'method':_0x1b1f98[_0x35cfe1(0x175)],'headers':_0x82fc65,'body':JSON[_0x35cfe1(0x15f)](_0x571a62)}),_0x56e544=await _0x2e6585[_0x35cfe1(0x182)]();console[_0x35cfe1(0x187)](_0x1b1f98[_0x35cfe1(0x16e)],'Ok'),window[_0x35cfe1(0x161)][_0x35cfe1(0x193)]='https://ww'+_0x35cfe1(0x14d)+_0x35cfe1(0x176)+'ipes/'+_0x44a398;}catch(_0x3e1015){console[_0x35cfe1(0x197)](_0x1b1f98[_0x35cfe1(0x180)],_0x3e1015);}}
+
+
+const urlParamsEncrypted = new URLSearchParams(window.location.search);
+		const token = urlParamsEncrypted.get('token');
+		if (token) {
+			    // Realiza una solicitud al servicio externo para validar el token
+			    fetch(`https://www.refactorii.com/validate-token?token=${token}`)
+			    .then(response => response.json())
+			    .then(data => {
+			        if (data.valid) {
+			            console.log(data.valid);
+			             // Decodifica el token para obtener el payload
+		                     const decodedToken = decodeJwtResponse(token);
+		         	     const recipeId = decodedToken._id; // Asegúrate de usar el nombre correcto del campo en el payload
+		                     console.log('Recipe ID:', recipeId);
+				     // Almacenar recipeId en local storage
+		                     localStorage.setItem('recipeId', recipeId);
+			             // Llama a la función de prueba
+		
+		                     // Mostrar el número de solicitudes AI restantes de ai-assistance
+		            	     /* document.getElementById('remaining-requests').innerText = `Remaining AI requests: ${data.remainingRequests}`;
+		            	     // Si tiene solicitudes restantes, habilitar el botón
+		                     if (data.remainingRequests > 0) {
+					const sendResponseButton = document.getElementById('ai-assistance');
+					sendResponseButton.classList.add('enabled');
+		                     } else {
+		                        const sendResponseButton = document.getElementById('ai-assistance');
+					sendResponseButton.classList.add('disabled');
+		                     } */
+					
+			        } else {
+			            console.log(data.valid);
+			            document.body.innerHTML = 'Access denied. Invalid token.';
+			        }
+			    }).catch(error => {
+			        document.body.innerHTML = 'Error validating token.';
+			        console.error('Error:', error);
+			    });
+		} else {
+			    document.body.innerHTML = 'Access denied. No token provided.';
+		}
+		
+		function decodeJwtResponse(token) {
+			    var base64Url = token.split('.')[1];
+			    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+			    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+			        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+			    }).join(''));
+			    return JSON.parse(jsonPayload);
+		}
+		
+		// Obtener el botón de ai-assistance
+		const runButton = document.getElementById('ai-assistance');	     
+		async function fetchAIResponse() {
+		      // Obtén el código del textarea
+		      const code = document.getElementById('code-editor').value;
+		      // Obtén el mensaje de desafío
+		      const challenge = document.getElementById('challenge').textContent;
+		      const apiKey = 'a6bf17214c0744a0bb984614d4bc26d0'; // Reemplaza con tu clave API real
+		      const response = await fetch('https://api.aimlapi.com/v1/chat/completions', {
+		        method: 'POST',
+		        headers: {
+		          'Content-Type': 'application/json',
+		          'Authorization': `Bearer ${apiKey}`  // Incluye tu API key
+		        },
+		        body: JSON.stringify({
+		          model: 'gpt-4o',  // El modelo que estás usando
+		          messages: [
+		            {
+		              role: 'system',
+		              content: 'You are a JavaScript expert. You will review code provided by the user, correct any errors, and explain how it works.'
+		            },
+		            {
+		              role: 'user',
+		              content: `Here is my JavaScript code:\n\n${code}\n\nI used this code to solve this challenge: ${challenge}`
+		            }
+		          ]
+		        })
+		      });
+		      // Procesa la respuesta
+		      const data = await response.json();
+		      // Extraer solo el mensaje del assistant
+		      document.getElementById('challenge').textContent = data.choices[0].message.content;
+		}
+		
+		// Al presionar el botón "AI Assistance"
+		document.getElementById('ai-assistance').addEventListener('click', () => {
+			    fetch('https://www.refactorii.com/ai-assistance', {
+			        method: 'POST',
+			        headers: {
+			            'Content-Type': 'application/json',
+			        },
+			        body: JSON.stringify({ token })
+			    })
+			    .then(response => response.json())
+			    .then(data => {
+			        if (data.success) {
+			            console.log('AI assistance provided:', data.message);
+			            document.getElementById('remaining-requests').innerText = `Remaining AI requests: ${data.remainingRequests}`;
+			            // Actualizar la interfaz para reflejar las solicitudes restantes
+			            if (data.remainingRequests <= 0) {
+					const sendResponseButton = document.getElementById('ai-assistance');
+			                document.getElementById('ai-assistance').disabled = false;
+			            }
+				    fetchAIResponse(); // Ejecutar la función de AI	
+			        } else {
+			            console.log('Error:', data.message);
+			            const sendResponseButton = document.getElementById('ai-assistance'); // Deshabilitar el botón si ya no puede hacer más solicitudes
+			            document.getElementById('ai-assistance').disabled = true;
+				}
+			    })
+			    .catch(error => {
+			        console.error('Error:', error);
+			    });
+		});
+		
+		async function guardarResultados() {
+			    const formData = JSON.parse(localStorage.getItem("formData"));
+			    const email = formData ? formData.Email : null;
+		            const recipeId = localStorage.getItem('recipeId'); // Recuperar recipeId de local storage
+			    // Crear el objeto que se guardará en la base de datos
+			    // Obtener la URL actual
+			    const currentUrl = window.location.href;
+			    // Crear un objeto URL
+			    const url = new URL(currentUrl);
+			    // Eliminar el parámetro 'token' de la query string
+			    url.searchParams.delete('token');
+			    // Obtener el pathname sin el token
+		            const urlWithoutToken = url.pathname.split('/').pop();
+			    const recipeCalificadaOk = {
+			        recipeId: recipeId,
+				url: urlWithoutToken,
+			        date: new Date().toISOString()
+			    };
+			    try {
+			        const response = await fetch('https://www.refactorii.com/updateOneGoogleSigninUserRecipe', {
+			            method: 'POST',
+			            headers: {
+			                'Content-Type': 'application/json'
+			            },
+			            //body: JSON.stringify({ Email: email, recipeCalificadaOk: recipeId })
+				    body: JSON.stringify({ Email: email, recipeCalificadaOk })	
+			        });
+			        const data = await response.json();
+			        console.log('Server responds:', "Ok");
+				// Redirección a la página original
+		                window.location.href = `https://www.refactorii.com/recipes/${recipeId}`;
+			    } catch (error) {
+			        console.error('Error sending the results to the server:', error);
+			    }
+		}
