9d2564991e87815ed501de6652f5acc711b0aaa2
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2025-03-13 16:41:16 -0500

    Update load_exercises.js

--- load_exercises.js
+++ load_exercises.js
@@ -3,16 +3,17 @@ let currentExerciseIndex = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
     const urlParams = new URLSearchParams(window.location.search);
-    const exerciseId = parseInt(urlParams.get("exercise"), 10); // Obtener el ejercicio de la URL
+    const exerciseId = parseInt(urlParams.get("exercise"), 10); // Asegurar que sea número
 
     if (!isNaN(exerciseId)) {
-        loadExercises(exerciseId); // Cargar el ejercicio al inicio
+        loadExercises(exerciseId);
     } else {
         console.error("No se encontró un ID de ejercicio válido en la URL.");
     }
 });
 
-function loadExercises(exerciseId) {   console.log(exerciseId)
+
+function loadExercises(exerciseId) {  
     fetch('exercises.json') // Cargar el JSON con los ejercicios
         .then(response => response.json())
         .then(exercises => {
