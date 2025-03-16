345207304091bd31b1cb88e5f829107a20ebbf27
3,1416 <85421189+wbsckt3@users.noreply.github.com>
2025-03-16 15:43:36 -0500

    Update token_functions.js

--- token_functions.js
+++ token_functions.js
@@ -169,7 +169,7 @@ const urlParamsEncrypted = new URLSearchParams(window.location.search);
 		                const challenge = document.getElementById('challenge').textContent;
 		                
 		                // Enviar código al backend para obtener respuesta de la IA
-		                const response = await fetch('https://www.refactorii.com/api/fetchAIResponseBack', {  // Corrección de URL
+		                const response = await fetch('https://www.refactorii.com/fetchAIResponseBack', {  // Corrección de URL
 		                    method: 'POST',
 		                    headers: {
 		                        'Content-Type': 'application/json'
